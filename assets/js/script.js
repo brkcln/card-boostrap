a05cc483479fe93482a7f82d8d9337e4a8cadf9c
Burak CELEN <burakcelen17@gmail.com>
2022-05-06 14:50:15 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
+let counts = setInterval(updated, 2000); // 3 saniye de bir
+let count = setInterval(update, 1000); // 2 saniye de bir
+let follow = 80;
+let likes = 803;
 
+//  follow function
+function updated() {
+	var counts = document.getElementById("card-follow");
+	counts.innerHTML = ++follow;
+	//  500 e gelince sıfırla
+	if (follow === 500) {
+		clearInterval(counts);
+	}
+}
+//  likes  function
+function update() {
+	var count = document.getElementById("card-likes");
+	count.innerHTML = ++likes;
+	//  1500 e gelince sıfırla
+	if (likes === 1500) {
+		clearInterval(count);
+	}
+}
